cd6fdc3dca6c493765d07db62224654dcc4b2bc8
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-20 10:35:57 -0300

    fix: remove unnecessary import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   constructor(private flowbiteService: FlowbiteService) {}
 
   ngOnInit(): void {
-    this.flowbiteService.loadFlowbite(flowbite => {
+    this.flowbiteService.loadFlowbite((flowbite) => {
       console.log('Flowbite loaded', flowbite);
     });
   }
