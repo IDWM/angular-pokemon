b88fd5dad6676a86eb52cb213d66355366dd3f42
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-20 22:18:05 -0300

    feat: use of pokemon service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from './services/flowbite.service';
+import { PokemonService } from './services/pokemon.service';
+import { Pokemon } from './interfaces/pokemon';
+import { TitleCasePipe } from '@angular/common';
+import { PokemonDetail } from './interfaces/pokemon-detail';
+import { PokemonData } from './interfaces/pokemon-data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  constructor(private flowbiteService: FlowbiteService) {}
+  pokemons: Pokemon[] = [];
+  pokemonDetails: PokemonDetail[] = [];
+
+  constructor(
+    private flowbiteService: FlowbiteService,
+    private pokemonService: PokemonService
+  ) {}
 
   ngOnInit(): void {
-    this.flowbiteService.loadFlowbite((flowbite) => {
-      console.log('Flowbite loaded', flowbite);
-    });
+    this.flowbiteService.loadFlowbite(() => {});
+
+    this.getPokemons();
+  }
+
+  getPokemons(): void {
+    const observer = {
+      next: (pokemonData: PokemonData) => {
+        this.pokemons = pokemonData.results;
+
+        for (const pokemon of this.pokemons) {
+          const parts = pokemon.url.split('/');
+          const idString = parts[parts.length - 2];
+          const id = parseInt(idString);
+
+          this.pokemonService.getPokemon(id).subscribe({
+            next: (pokemonDetailData) => {
+              this.pokemonDetails.push(pokemonDetailData);
+            },
+            error: (error) => {
+              console.error('Error fetching pokemon details:', error);
+            },
+          });
+        }
+      },
+      error: (error: any) => {
+        console.error('Error fetching pokemons:', error);
+      },
+    };
+
+    this.pokemonService.getPokemons().subscribe(observer);
   }
 }
